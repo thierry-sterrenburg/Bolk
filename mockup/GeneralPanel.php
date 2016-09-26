8bee8539a45be094d02da757eb93e1e948f5da69
camilio <cdelfgaauw@gmail.com>
2016-09-26 09:53:40 +0200

    Added general panel mockup

---
+++ mockup/GeneralPanel.php
@@ -0,0 +1,56 @@
+		<!--General information panel-->				
+        <div id="page-wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+						<ol class="breadcrumb">
+							<li><a href="projects.html">Projects</a></li>
+							<li><a href="project.html">Project GE Auchrobert</a></li>
+							<li><a href="windmill.html">Windmill T11</a></li>
+							<li class="active">PPM</li>
+						</ol>
+                        <h1 class="page-header">PPM</h1>
+						
+						<!--panel content -->
+						
+						
+						<div class="panel panel-default">
+							<div class="panel-heading">
+								<h3 class="panel-title">General Information</h3>
+							</div>
+							<div class="panel-body">
+								Project GE Auchrobert registration number:189207<br/>
+								latest update: 13-9-2016 13:52:07 <br/>
+								number of phases: 3<br/>
+								weight:<br/>
+								height:<br/>
+								width:<br/>
+								length:<br/>
+								
+							</div>
+						</div>
+						
+						<br>
+						
+						<ul class="nav nav-pills" role="tablist">
+							<li role="presentation" class="active"><a href="#">Add Transport Phase<span class="badge">+</span></a></li>
+						</ul>
+						
+						<br>
+						
+						
+							
+							<tbody>
+								
+								<p> Tabel hier </p>
+							</tbody>
+							
+                    </div>
+					
+                    <!-- /.col-lg-12 -->
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+        </div>
+        <!-- /#page-wrapper -->
