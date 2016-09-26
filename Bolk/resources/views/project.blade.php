acfb6a09b53c0a7ab377caf9a2be967c137662d7
ChrisVasShiver <c_visscher@hotmail.com>
2016-09-26 15:27:02 +0200

    added new table basics

--- Bolk/resources/views/project.blade.php
+++ Bolk/resources/views/project.blade.php
@@ -11,9 +11,7 @@
 						</ol>
                         <h1 class="page-header">Project GE Auchrobert</h1>
 						
-						<!--panel content -->
-						
-						
+						<!--panel content -->						
 						<div class="panel panel-default">
 							<div class="panel-heading">
 								<h3 class="panel-title">General Information</h3>
@@ -30,7 +28,8 @@
 						</ul>
 						
 						<br>
-						
+						<!--Windmill Table -->
+						<h3>Windmills</h3>
 						<table class="table table-condensed table-hover">
 							<thead>
 								<td>#</td>
@@ -58,6 +57,42 @@
 										<td>{{ $windmill->remarks }}</td>
 									</tr>
 								@endforeach	
+							</tbody>
+							
+						</table>
+						<!-- Component Table-->
+						<h3>Components</h3>
+						<table class="table table-condensed table-hover">
+							<thead>
+								<td>#</td>
+								<td>Reg. number</td>
+								<td>Name</td>
+								<td>From</td>
+								<td>To</td>
+								<td>Number of transport phases</td>
+								<td>Date of loading</td>
+								<td>Date of Arrival</td>
+								<td>Offloading(initial)</td>
+								<td>Offloading(final)</td>
+								<td>Last update</td>
+								<td>Remarks</td>
+							</thead>
+							
+							<tbody>
+									<tr>
+										<td></td>
+										<td></td>
+										<td></td>
+										<td></td>
+										<td></td>
+										<td></td>
+										<td></td>
+										<td></td>
+										<td></td>
+										<td></td>
+										<td></td>
+										<td></td>
+									</tr>	
 							</tbody>
 							
 						</table>
