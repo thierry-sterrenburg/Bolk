a3087b95a3e5bf0e61f2943aabcbfb5d97523823
camilio <cdelfgaauw@gmail.com>
2016-09-28 12:30:37 +0200

    added general panel

---
+++ Bolk/resources/views/GeneralPanel.blade.php
@@ -0,0 +1,143 @@
+@extends('layouts.master')
+@section('content')
+		<!--General information panel-->				
+        <div id="page-wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+						<ol class="breadcrumb">
+							<li><a href="projects.html">Projects</a></li>
+							<li><a href="project.html">Project GE Auchrobert</a></li>
+							<li><a href="windmill.html">Windmill T11</a></li>
+							<li class="active">PPM</li>
+						</ol>
+                        <h1 class="page-header">PPM</h1>
+						
+						<!--panel content -->
+						
+						
+						<div class="panel panel-default">
+							<div class="panel-heading">
+								<h3 class="panel-title">General Information windmill</h3>
+							</div>
+							<div class="panel-body">
+								Project name<br/>
+								Location<br/>
+								Start date<br/>
+								End date<br/>
+								Remarks
+								
+							</div>
+						</div>
+						
+						<br>
+						<div class="panel panel-default">
+							<div class="panel-heading">
+								<h3 class="panel-title">General Information component</h3>
+							</div>
+							<div class="panel-body">
+								<div class="panel-left">
+									Windmillnr<br/>
+									Name<br/>
+									Location<br/>
+									Start date<br/>
+									End date
+								</div>
+								<div class="panel-right">
+									Project name<br/>
+									location<br/>
+									Start date<br/>
+									End date<br/>
+									Remarks
+								</div>
+								
+							</div>
+						</div>
+						<br>
+						<div class="panel panel-default">
+							<div class="panel-heading">
+								<h3 class="panel-title">General Information transport</h3>
+							</div>
+							<div class="panel-body">
+								<div class="panel-left">
+									Component nr<br/>
+									Name<br/>
+									Length<br/>
+									Height<br/>
+									Width<br/>
+									Weigth<br/>
+									Switchable<br/>
+									Status<br/>
+									Remarks
+								</div>
+								<div class="panel-right">
+									Project name<br/>
+									Windmill name<br/>
+									Location<br/>
+									Start date<br/>
+									End date<br/>
+									Remarks
+								</div>
+								
+							</div>
+						</div>
+						<br>
+						<div class="panel panel-default">
+							<div class="panel-heading">
+								<h3 class="panel-title">General Information requiremets</h3>
+							</div>
+							<div class="panel-body">
+								<div class="panel-left">
+									Company<br/>
+									Truck<br/>
+									Trailer<br/>
+									Configuration<br/>
+									Start location <br/>
+									End location <br/>
+									Date loading <br/>
+									Date intial arrival <br/>
+									Date final arrival <br/>
+									Remarks <br/>
+
+								</div>
+								<div class="panel-right">
+									Project name<br/>
+									Windmill name<br/>
+									Location<br/>
+									Component nr<br/>
+									Name<br/>
+									Length<br/>
+									Height<br/>
+									Width<br/>
+									Weigth<br/>
+									Switchable<br/>
+									Status<br/>
+									Remarks
+								</div>
+								
+							</div>
+						</div>
+						<br>
+						
+						<ul class="nav nav-pills" role="tablist">
+							<li role="presentation" class="active"><a href="#">Add Transport Phase<span class="badge">+</span></a></li>
+						</ul>
+						
+						<br>
+						
+						
+							
+							<tbody>
+								
+								<p> Tabel hier </p>
+							</tbody>
+							
+                    </div>
+					
+                    <!-- /.col-lg-12 -->
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+        </div>
+        <!-- /#page-wrapper -->
