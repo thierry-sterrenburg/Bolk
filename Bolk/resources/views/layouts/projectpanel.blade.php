586a4d7dd081682401a66e3de9bb2cdf50c4e52c
ChrisVasShiver <c_visscher@hotmail.com>
2016-10-03 15:58:47 +0200

    push generalinformation on almost all views

---
+++ Bolk/resources/views/layouts/projectpanel.blade.php
@@ -0,0 +1,46 @@
+<?php 
+	use App\Http\Controllers\ProjectsController;
+?>
+						<!--panel projectinfo -->
+						<div class="panel-group">
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<div class="row">
+										<div class="col-md-8">
+											<a data-toggle="collapse" href="#projectinfotable">
+												<h3 class="panel-title">Project Information </h3>
+											</a>
+										</div>
+										<div class="col-md-4">
+											<div class="pull-right">
+												<small>Latest Update:</small>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div id="projectinfotable" class="panel-collapse collapse">
+									<table class="table table-bordered">
+										<tbody>
+											<tr>
+												<td colspan="1"><u>Project Registration Number:</u> {{$project->regnumber}}</td>
+												<td colspan="1"><u>Name: {{$project->name}}</u></td>
+												<td colspan="1"><u>Location:</u> {{$project->location}}</td>
+											</tr>
+											<tr>
+												<td colspan="1"><u>Start Date:</u> {{$project->startdate}}</td>
+												<td colspan="2"><u>End Date:</u> {{$project->enddate}}</td>
+											</tr>
+											<tr>
+												<td colspan="1"><u>Total number of Windmills:</u> {{ProjectsController::countWindmills($project->id)}}</td>
+												<td colspan="1"><u>Total number of Components:</u> {{ProjectsController::countComponents($project->id)}}</td>
+												<td colspan="1"><u>Total number of Transport Phases:</u> {{ProjectsController::countTransports($project->id)}}</td>
+											</tr>
+											<tr>
+												<td colspan="3"><u>Remarks:</u>{{$project->remarks}}</td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
+							</div>
+						</div>
+						<!--end panel projectinfo-->
