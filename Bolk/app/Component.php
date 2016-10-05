00d18b502792fc9b736856bb2c618e999248010b
thierry-sterrenburg <thierry-sterrenburg@live.nl>
2016-10-05 09:49:26 +0200

    Fillables in Component

--- Bolk/app/Component.php
+++ Bolk/app/Component.php
@@ -8,6 +8,20 @@ class Component extends Model
 {
     protected $table = 'components';
     protected $touches = ['project','windmill'];
+	protected $fillable=[
+		'projectid',
+		'windmillid',
+		'regnumber',
+		'name',
+		'length',
+		'height',
+		'width',
+		'weight',
+		'switchable',
+		'currentlocation',
+		'status',
+		'remarks'		
+	];
 
     public function projects() {
     	return $this->belongsTo('App\Project', 'projectid');
