5ae9b89034633811c9968216c2780e08e3fcdb32
camilio <cdelfgaauw@gmail.com>
2016-10-06 14:23:40 +0200

    The added javascript for Treeview

--- Bolk/app/Providers/AppServiceProvider.php
+++ Bolk/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Project;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        view()->composer('partials.nav', function ($view)
+        {
+            $projects = Project::pluck('id', 'name');
+            $view->with('projects', $projects);
+        });
     }
 
     /**
