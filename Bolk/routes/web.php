c41e6649fe808882a136f829f49729d88ffdbba4
thierry-sterrenburg <thierry-sterrenburg@live.nl>
2016-10-11 12:30:29 +0200

    Route fix

--- Bolk/routes/web.php
+++ Bolk/routes/web.php
@@ -23,34 +23,34 @@ Route::get('/index', function () {
 //    return view('clients');
 //});
 
-Route::get('/projects', ['middleware' => ['permission:read-project'], 'uses' => 'ProjectsController@index']);
-Route::post('/newProject', ['middleware' => ['permission:create-project'], 'uses' => 'ProjectsController@newProject']);
-Route::get('/getUpdate', ['middleware' => ['permission:read-project'], 'uses'=>'ProjectsController@getUpdate']);
-Route::put('/newProject', ['middleware' => ['permission:create-project'], 'uses' => 'ProjectsController@newUpdate']);
-Route::delete('/deleteProject', ['middleware' => ['permission:delete-project'], 'uses' => 'ProjectsController@deleteProject']);
+Route::get('/projects', 'ProjectsController@index');
+Route::post('/newProject', 'ProjectsController@newProject');
+Route::get('/getUpdate', 'ProjectsController@getUpdate');
+Route::put('/newProject', 'ProjectsController@newUpdate');
+Route::delete('/deleteProject', 'ProjectsController@deleteProject');
 
-Route::get('/project/id={id}', ['middleware' => ['permission:read-project'], 'uses' => 'ProjectController@index']);
+Route::get('/project/id={id}', , 'uses' => 'ProjectController@index');
 Route::get('/project_transport/id={id}','Project_transportController@index');
-Route::post('/newWindmill', ['middleware' => ['permission:create-windmill'], 'uses' => 'ProjectController@newWindmill']);
-Route::get('/getUpdateWindmill', ['middleware' => ['permission:read-windmill'], 'uses' => 'ProjectController@getUpdateWindmill']);
-Route::put('/newWindmill', ['middleware' => ['permission:edit-windmill'], 'uses' => 'ProjectController@newUpdateWindmill']);
-Route::delete('/deleteWindmill', ['middleware' => ['permission:delete-windmill'], 'uses' => 'ProjectController@deleteWindmill']);
+Route::post('/newWindmill', 'ProjectController@newWindmill');
+Route::get('/getUpdateWindmill', 'ProjectController@getUpdateWindmill');
+Route::put('/newWindmill', 'ProjectController@newUpdateWindmill');
+Route::delete('/deleteWindmill', 'ProjectController@deleteWindmill');
 
-Route::post('/newComponent', ['middleware' => ['permission:create-component'], 'uses' => 'ProjectController@newComponent']);
-Route::get('/getUpdateComponent', ['middleware' => ['permission:read-component'], 'uses' => 'ProjectController@getUpdateComponent']);
-Route::put('/newComponent', ['middleware' => ['permission:edit-component'], 'uses' => 'ProjectController@newUpdateComponent']);
-Route::delete('/deleteComponent', ['middleware' => ['permission:delete-component'], 'uses' => 'ProjectController@deleteComponent']);
-View::composer('/newComponent', ['middleware' => ['permission:create-component'], 'uses' => 'App\Http\ViewComposers\newComponentComposer']);
+Route::post('/newComponent', 'ProjectController@newComponent');
+Route::get('/getUpdateComponent', 'ProjectController@getUpdateComponent');
+Route::put('/newComponent', 'ProjectController@newUpdateComponent');
+Route::delete('/deleteComponent','ProjectController@deleteComponent');
+View::composer('/newComponent', 'App\Http\ViewComposers\newComponentComposer');
 
-Route::get('/windmill/id={id}', ['middleware' => ['permission:read-windmill'], 'uses' => 'WindmillController@index']);
+Route::get('/windmill/id={id}', 'WindmillController@index');
 
-Route::get('/component/id={id}', ['middleware' => ['permission:read-component'], 'uses' => 'ComponentController@index']);
-Route::post('/newTransport', ['middleware' => ['permission:create-transport'], 'uses' => 'ComponentController@newTransport']);
-Route::get('/getUpdateTransport', ['middleware' => ['permission:read-transport'], 'uses' => 'ComponentController@getUpdateTransport']);
-Route::put('/newTransport', ['middleware' => ['permission:edit-transport'], 'uses' => 'ComponentController@newUpdateTransport']);
-Route::delete('/deleteTransport', ['middleware' => ['permission:delete-transport'], 'uses' => 'ComponentController@deleteTransport']);
+Route::get('/component/id={id}', 'ComponentController@index');
+Route::post('/newTransport','ComponentController@newTransport');
+Route::get('/getUpdateTransport', 'ComponentController@getUpdateTransport');
+Route::put('/newTransport', 'ComponentController@newUpdateTransport');
+Route::delete('/deleteTransport', 'ComponentController@deleteTransport');
 
-Route::get('/transportphase/id={id}', ['middleware' => ['permission:read-transport'], 'uses' => 'TransportphaseController@index']);
+Route::get('/transportphase/id={id}','TransportphaseController@index');
 
 Route::get('/closestdeadlines', function () {
     return view('closestdeadlines');
