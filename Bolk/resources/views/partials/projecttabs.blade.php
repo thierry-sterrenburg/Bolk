8f9d9b74d72a037fc2990f5fd77ae5583d68b437
ChrisVasShiver <c_visscher@hotmail.com>
2016-10-12 15:28:38 +0200

    projecttabs working

--- Bolk/resources/views/partials/projecttabs.blade.php
+++ Bolk/resources/views/partials/projecttabs.blade.php
@@ -1,6 +1,19 @@
 <ul class="nav nav-tabs">
-	<li><a onclick="document.location= '/project/id={{$project->id}}';">Windmills</a></li>
-	<li><a onclick="document.location= '/project_components/id={{$project->id}}';">Components</a></li>
-	<li><a onclick="document.location= '/project_transports/id={{$project->id}}';">Transports</a></li>
+	<li id="projecttab"><a onclick="document.location= '/project/id={{$project->id}}';">Windmills</a></li>
+	<li id="componenttab"><a onclick="document.location= '/project_components/id={{$project->id}}';">Components</a></li>
+	<li id="transporttab"><a onclick="document.location= '/project_transports/id={{$project->id}}';">Transports</a></li>
 	<li><a href="#">Menu 3</a></li>
 </ul>
+
+<script type=text/javascript>
+	var url = document.location.toString();
+	if (url.match('/project/id=')) {
+    	$('#projecttab').addClass('active');
+   	} 
+   	else if (url.match('/project_components/id=')) {
+   		$('#componenttab').addClass('active');
+   	};
+   	else if (url.match('/project_transports/id=')) {
+   		$('#transporttab').addClass('active');
+   	};
+</script>
