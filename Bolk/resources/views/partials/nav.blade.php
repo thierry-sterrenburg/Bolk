e0ddcc847bd9fef6949d9cc335a700dec5ed2744
Yuri van Midden <mail@yurivanmidden.nl>
2016-10-31 12:00:08 +0100

    Fixed an oopsje in de navbar.

--- Bolk/resources/views/partials/nav.blade.php
+++ Bolk/resources/views/partials/nav.blade.php
@@ -29,7 +29,7 @@ use app\Http\Controllers\NavController;
 		$sizeP = count($projectsoverview);
 		$indexP = 1; 
 		foreach($projectsoverview as $project){
-            if(!(Auth::user()->projectid == '') && Auth::user()->projectid == $project->id){
+            if(Auth::user()->projectid == $project->id){
 			echo '{ 
 				text: '.json_encode($project->name).', 
 				icon: "fa fa-institution",
@@ -128,7 +128,107 @@ use app\Http\Controllers\NavController;
 			},';
 			$indexP ++;
 		}
-	}}
+	}
+		elseif(Auth::user()->projectid == ''){
+            echo '{
+				text: '.json_encode($project->name).',
+				icon: "fa fa-institution",
+				href: "../project/id='.$project->id.'"';
+            $indexW = 1;
+            $sizeW = count($windmilloverview);
+            $firstW = True;
+            foreach($windmilloverview as $windmill){
+                if($project->id == $windmill->projectid){
+                    if ($firstW){
+                        echo ', nodes:
+						[
+						';
+                        $firstW = False;
+                    }else{
+                        echo ',
+						';
+                    }
+                    echo '{
+					text: '.json_encode($windmill->name).',
+					icon: "fa fa-tencent-weibo",
+					href: "../windmill/id='.$windmill->id.'"';
+                    $indexC = 1;
+                    $sizeC = count($componentoverview);
+                    $firstC = True;
+                    foreach($componentoverview as $component){
+                        if(!is_null($component->mainwindmillid)&&($windmill->id == $component->mainwindmillid)){
+                            if ($firstC){
+                                echo ', nodes:
+							[
+							';
+                                $firstC = False;
+                            }else{
+                                echo ',
+							';
+                            }
+                            echo '{text: '.json_encode($component->name).',
+							icon: "fa fa-archive",
+							href: "../component/id='.$component->id.'"}';
+                        }
+                        if((!$firstC)&&($indexC == $sizeC)){
+                            echo '
+							]
+						';
+                        }
+                        $indexC ++;
+                    }
+                    if((!$firstW)){
+                        echo '
+					}
+					';
+                    }
+                }
+                $indexW ++;
+            }
+
+            $indexC = 1;
+            $sizeC = count($componentoverview);
+            $firstC = True;
+            foreach($componentoverview as $component){
+                if(is_null($component->mainwindmillid)&&$component->projectid == $project->id){
+                    if($firstC){
+                        if ($firstW) {
+                            echo ', nodes:
+				[
+				';
+                        }else{
+                            echo ',
+				';
+                        }
+                        $firstC = False;
+                    }else{
+                        echo ',
+				';
+                    }
+                    echo'{
+				text: '.json_encode($component->name).',
+				icon: "fa fa-archive",
+				href: "../component/id='.$component->id.'"
+				}
+				';
+                }
+                if(((!$firstC)||(!$firstW))&&($indexC == $sizeC)){
+                    echo ']
+					';
+                }
+                $indexC++;
+            }
+
+
+            if($indexP == $sizeP){
+                echo '
+			}';
+            }else{
+                echo '
+			},';
+                $indexP ++;
+            }
+        }}
 	?>
 	]
 	},
